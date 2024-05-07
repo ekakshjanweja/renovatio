ac9a174efe0c8f3ca0210d623ea8e8c574dc06aa
ekakshjanweja <jekaksh@gmail.com>
2024-05-07 15:12:59 +0530

    fixing image uploads on prod

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,3 @@
-import db from "@/db";
-import { projects } from "@/db/schema/projects";
-import { getCurrentUser } from "@/services/user-service";
-import { eq } from "drizzle-orm";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -12,40 +8,9 @@ export const ourFileRouter = {
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
-  })
-    .middleware(async ({ req }) => {
-      const user = await getCurrentUser();
-
-      return {
-        user: user,
-      };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      return { fileUrl: file.url };
-    }),
-
-  projectImageUploader: f({
-    image: {
-      maxFileSize: "4MB",
-      maxFileCount: 10,
-    },
-  })
-    .middleware(async () => {
-      const user = await getCurrentUser();
-
-      return { user: user };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      await db
-
-        .update(projects)
-        .set({
-          imageModel: [file.url],
-        })
-        .where(eq(projects.designerId, metadata.user.id));
-
-      return { fileUrl: file.url };
-    }),
+  }).onUploadComplete(async ({ file }) => {
+    return { fileUrl: file.url };
+  }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
