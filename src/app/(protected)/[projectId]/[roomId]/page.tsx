5ec78592f456d70f41ead24b9261def536281897
ekakshjanweja <jekaksh@gmail.com>
2024-07-01 10:59:01 +0530

    more ui for room page

--- src/app/(protected)/[projectId]/[roomId]/page.tsx
+++ src/app/(protected)/[projectId]/[roomId]/page.tsx
@@ -4,6 +4,8 @@ import { notFound } from "next/navigation";
 import { AutoScrollCarousel } from "../_components/auto-scroll-carousel";
 import { UploadDropzone } from "@/lib/uploadthing";
 import { UploadRoomImagesComponent } from "./_components/upload-room-images-component";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
 
 interface RoomPageProps {
   params: {
@@ -24,8 +26,37 @@ const RoomPage = async ({ params }: RoomPageProps) => {
     <>
       <div className="p-6">
         <AutoScrollCarousel images={room.images} isFull />
+
+        <div className="flex flex-col my-4 p-4 border border-muted-foreground border-dotted rounded-lg space-y-4">
+          <div className="flex justify-between items-center">
+            <p className="text-3xl font-semibold">{room.name}</p>
+            <Button variant={"outline"}>
+              <Pencil />
+            </Button>
+          </div>
+          <div>
+            <p className="text-xl font-medium">Room Description</p>
+            <p>
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume
+              Lorem Ipsum Lorem Ipsume Lorem Ipsum Lorem Ipsume Lorem Ipsum
+              Lorem Ipsume
+            </p>
+          </div>
+          <div></div>
+        </div>
+
         <UploadRoomImagesComponent roomId={roomId} />
-        {room.name}
       </div>
     </>
   );
