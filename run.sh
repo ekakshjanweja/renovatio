1660b2c46576b450010f7fb0fb23d19a29d53546
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-07-03 12:02:05 +0530

    removed --rm flag from docker run
    removed --rm flag as it removed the docker container

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 docker build -t renovatio_image .
 
 # runs the built image,
-#  --rm: removes the container when exit
 #  --env-file .env : passes the .env file to the container
-docker run --rm -p 3000:3000 --env-file .env --name renovatio renovatio_image
+docker run -p 3000:3000 --env-file .env --name renovatio renovatio_image
