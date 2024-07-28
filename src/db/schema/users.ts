1ae197369149d9e7866e40816e048d980ab2617c
ekakshjanweja <jekaksh@gmail.com>
2024-07-28 17:01:10 +0530

    update user schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,4 +1,10 @@
-import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("user", {
   id: text("id").notNull().primaryKey(),
@@ -8,6 +14,7 @@ export const users = pgTable("user", {
   image: text("image").notNull(),
   phone: text("phone"),
   isDesigner: boolean("isDesigner").default(false),
+  remaining: integer("remaining").notNull().default(100),
 
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
