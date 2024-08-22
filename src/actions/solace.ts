d2bfdd264c3297ce601815f33b94b43ef1b12aed
Ekaksh Janweja <jekaksh@gmail.com>
2024-08-22 13:54:10 +0530

    remove commented code

--- src/actions/solace.ts
+++ src/actions/solace.ts
@@ -20,30 +20,8 @@ export const generateImage = async (prompt: string) => {
     return null;
   }
 
-  // const url = "https://cloud.leonardo.ai/api/rest/v1/generations";
-
   const leonardoKinoXL = "aa77f04e-3eec-4034-9c07-d0f619684628";
   const leonardoPhoenix = "6b645e3a-d64f-4341-a6d8-7a3690fbf042";
-
-  // const options = {
-  //   method: "POST",
-  //   headers: {
-  //     accept: "application/json",
-  //     "content-type": "application/json",
-  //     authorization: `Bearer 45c614de-891d-497b-a952-818c84b97d87`,
-  //   },
-  //   body: JSON.stringify({
-  //     alchemy: true,
-  //     height: 1024,
-  //     width: 1024,
-  //     modelId: leonardoKinoXL,
-  //     num_images: 1,
-  //     presetStyle: "CINEMATIC",
-  //     prompt: prompt,
-  //     photoReal: true,
-  //     promptMagic: false,
-  //   }),
-  // };
 
   const result = await leonardo.image.createGeneration({
     // alchemy: true,
@@ -68,21 +46,6 @@ export const generateImage = async (prompt: string) => {
     job?.sdGenerationJob?.generationId!
   );
 
-  // const response = await fetch(url, options);
-
-  // if (!response.ok) {
-  //   console.log(response);
-
-  //   console.log("Failed to initiate image generation.");
-  //   return null;
-  // }
-
-  // const data = await response.json();
-
-  // const job: ImageGenerationJob = data["sdGenerationJob"];
-
-  // const generationResponse = await getGenerations(job.generationId);
-
   if (generationResponse["data"] === "error") {
     console.log("failed to get generated image.");
     return null;
@@ -91,11 +54,6 @@ export const generateImage = async (prompt: string) => {
   const generations = generationResponse[
     "data"
   ] as GetGenerationByIdResponseBody;
-
-  // if (generations === undefined) {
-  //   console.log("Failed to get generated image.");
-  //   return null;
-  // }
 
   const generatedImages = generations.generationsByPk?.generatedImages;
 
@@ -141,37 +99,6 @@ export const getGenerations = async (generationId: string) => {
 
       generations = result["data"] as GetGenerationByIdResponseBody;
     }
-
-    // const options = {
-    //   method: "GET",
-    //   headers: {
-    //     accept: "application/json",
-    //     authorization: `Bearer ${apiKey}`,
-    //   },
-    // };
-
-    // const response = await fetch(url, options);
-
-    // if (!response.ok) {
-    //   throw new Error("Failed to get generated image.");
-    // }
-
-    // const data = await response.json();
-
-    // let generations: Generations = data["generations_by_pk"];
-
-    // //TODO: BAD Implementation -> try with a webhook
-    // //Kindda Like Polling
-
-    // if (generations.generated_images.length === 0) {
-    //   const result = await getGenerations(generationId);
-
-    //   if (result["status"] === "error") {
-    //     throw result["data"];
-    //   }
-
-    //   generations = result["data"] as Generations;
-    // }
 
     return {
       status: "success",
