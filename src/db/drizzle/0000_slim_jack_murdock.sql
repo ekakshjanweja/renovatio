d4be9bb5d77d3b5d65c097744b8782dd2b05ccc8
silverstone-git <silverstone965@gmail.com>
2024-10-23 14:22:58 +0530

    db fixes

--- src/db/drizzle/0000_slim_jack_murdock.sql
+++ src/db/drizzle/0000_slim_jack_murdock.sql
@@ -13,6 +13,16 @@ CREATE TABLE IF NOT EXISTS "account" (
 	CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId")
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "bills" (
+	"id" uuid DEFAULT gen_random_uuid(),
+	"item" text,
+	"category" text,
+	"status" integer,
+	"amount" numeric,
+	"designer_id" text,
+	"client_id" text
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "designers" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
@@ -34,8 +44,10 @@ CREATE TABLE IF NOT EXISTS "projects" (
 	"area" integer NOT NULL,
 	"description" text NOT NULL,
 	"category" text NOT NULL,
+	"image_model" text[] NOT NULL,
 	"designer_id" text,
 	"user_id" text,
+	"status" text DEFAULT 'draft' NOT NULL,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
@@ -43,7 +55,9 @@ CREATE TABLE IF NOT EXISTS "projects" (
 CREATE TABLE IF NOT EXISTS "rooms" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
+	"description" text NOT NULL,
 	"image_model" text[] NOT NULL,
+	"image_for_generation" text,
 	"projectid" text NOT NULL
 );
 --> statement-breakpoint
@@ -51,6 +65,31 @@ CREATE TABLE IF NOT EXISTS "session" (
 	"sessionToken" text PRIMARY KEY NOT NULL,
 	"userId" text NOT NULL,
 	"expires" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "solace" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"prompt" text NOT NULL,
+	"model" "model" DEFAULT 'leonardo_kino_xl' NOT NULL,
+	"cover_url" text NOT NULL,
+	"contextual_image_url" text,
+	"style" "style" DEFAULT 'minimalist' NOT NULL,
+	"room_type" "room_type" DEFAULT 'kitchen' NOT NULL,
+	"number_of_images" integer DEFAULT 1 NOT NULL,
+	"is_enhanced" boolean DEFAULT false NOT NULL,
+	"variations" jsonb NOT NULL,
+	"generated_by" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "todos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"category" text,
+	"depends_on" integer,
+	"status" integer,
+	"description" text,
+	"designer_id" text
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user" (
@@ -61,6 +100,8 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"image" text NOT NULL,
 	"phone" text,
 	"isDesigner" boolean DEFAULT false,
+	"remaining" integer DEFAULT 100 NOT NULL,
+	"isPro" boolean DEFAULT false,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
@@ -72,18 +113,25 @@ CREATE TABLE IF NOT EXISTS "verificationToken" (
 	CONSTRAINT "verificationToken_identifier_token_pk" PRIMARY KEY("identifier","token")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "bills" (
-	"id" uuid DEFAULT gen_random_uuid(),
-	"item" text,
-	"category" text,
-	"status" integer,
-	"amount" numeric,
-	"designer_id" text,
-	"client_id" text
+CREATE TABLE IF NOT EXISTS "waitlist" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL
 );
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "bills" ADD CONSTRAINT "bills_designer_id_user_id_fk" FOREIGN KEY ("designer_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "bills" ADD CONSTRAINT "bills_client_id_user_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -119,13 +167,19 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "bills" ADD CONSTRAINT "bills_designer_id_user_id_fk" FOREIGN KEY ("designer_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "solace" ADD CONSTRAINT "solace_generated_by_user_id_fk" FOREIGN KEY ("generated_by") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "bills" ADD CONSTRAINT "bills_client_id_user_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "todos" ADD CONSTRAINT "todos_depends_on_todos_id_fk" FOREIGN KEY ("depends_on") REFERENCES "public"."todos"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "todos" ADD CONSTRAINT "todos_designer_id_user_id_fk" FOREIGN KEY ("designer_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
